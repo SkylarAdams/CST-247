8a4d0d022fa271bf08d334479277c32b353f3a63
Skylar Adams <sadams52@my.gcu.edu>
2018-02-04 22:03:09 -0700

    In Class Activity 4

---
+++ Activity4/Activity4/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using Activity4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity4.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        [HttpGet]
+        public ActionResult Index()
+        {
+            //initialize list of users to display
+            List<UserModel> users = new List<UserModel>();
+            users.Add(new UserModel("Mark Reha", "Mark@gmail.com", "911-234-2355"));
+            users.Add(new UserModel("Skylar Adams", "Skylar@gmail.com", "912-124-7424"));
+            users.Add(new UserModel("Trevor Moore", "Trevor@gmail.com", "913-233-5352"));
+            users.Add(new UserModel("Jabril", "bearssuck@gmail.com", "914-346-4335"));
+
+            //display users
+            return View("Test", users);
+        }
+    }
+}
---
+++ Activity4/Activity4/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Activity4.Models
+{
+    public class UserModel
+    {
+
+        public string Name { set; get; }
+        public string EmailAddress { set; get; }
+        public string PhoneNumber { set; get; }
+
+        public UserModel(string Name, string Email, string Phone)
+        {
+            this.Name = Name;
+            this.EmailAddress = Email;
+            this.PhoneNumber = Phone;
+        }
+    }
+}
