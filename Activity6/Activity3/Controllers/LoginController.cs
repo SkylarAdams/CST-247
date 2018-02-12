4e06e73507f0a248582a25303d07980bb3bcd15d
Skylar Adams <sadams52@my.gcu.edu>
2018-02-12 16:03:18 -0700

    In Class Activity 6 using Activity 3

---
+++ Activity6/Activity3/Controllers/LoginController.cs
@@ -0,0 +1,46 @@
+﻿using Activity3.Models;
+using Activity3.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        //[HttpPost]
+        //public String Login(UserModel model)
+        //{
+        //    return @"Here " + model.Username + " and " + model.Password;
+        //}
+
+        [HttpPost]
+        public ActionResult Login (UserModel model)
+        {
+
+            //Validate form POST
+            if (!ModelState.IsValid)
+                return View("Login");
+
+            //Use the Security Service to authenticate the user
+            SecurityService service = new SecurityService();
+            if (service.Authenticate(model))
+                return View("LoginPassed");
+            else
+                return View("LoginFailed", model);
+
+        }
+
+
+
+    }
+}
---
+++ Activity6/Activity3/Services/Data/SecurityDAO.cs
@@ -0,0 +1,57 @@
+﻿using Activity3.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Activity3.Services.Data
+{
+    public class SecurityDAO
+    {
+        string connectionString =
+        "Data Source=(localdb)\\MSSQLLocalDB; initial catalog = Test; Integrated Security = True";
+            
+            //USER ID=Skylar.j.adams@gmail.com; Password=Titleist36";
+
+            public bool FindByUser(UserModel user)
+            {
+            bool result = false;
+            
+
+            try
+            {
+                string query = "SELECT * FROM dbo.users WHERE USERNAME = @Username AND PASSWORD = @PASSWORD";
+
+                using (SqlConnection cn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand(query, cn))
+                {
+                    cmd.Parameters.Add("@Username", SqlDbType.VarChar, 50).Value = user.Username;
+                    cmd.Parameters.Add("@Password", SqlDbType.VarChar, 50).Value = user.Password;
+
+                    //open db connection
+                    cn.Open();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                        result = true;
+                    else
+                        result = false;
+
+                    //close db connection
+                    cn.Close();
+
+                }
+            }
+            catch (SqlException e)
+            {
+                throw e;
+            }
+
+            return result;
+
+        }
+       
+    }
+}
